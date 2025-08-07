f9f7ddf1adb53eb23f9fb32a15957dd91c46528d
Taylor <69871115+tkerbe2@users.noreply.github.com>
2025-08-06 21:40:26 -0500

    adding bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+yum install python3 -y
+yum install python3-pip -y
+python3 -m pip install simple_http_server -y
+yum install git -y
+git clone https://github.com/tkerbe2/iac-intro-lab-5
+cd iac-intro-lab-5/modules/web-app/
+chmod 700 bash-html-create.sh
+./bash-html-create.sh
+python3 -m http.server 80 >> landing.html
